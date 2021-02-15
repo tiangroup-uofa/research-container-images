a18c5a26e99621ee52498c5a29c52c060414dfae
T.Tian <alchem0x2a@gmail.com>
2021-02-15 11:58:36 -0500

    modify custom script for ssh-tunnel

--- ssh-tunnel/config/custom-cont-init.d/00-sshd-patch.sh
+++ ssh-tunnel/config/custom-cont-init.d/00-sshd-patch.sh
@@ -3,4 +3,5 @@
 # Update the sshd config to allow TCP & X11 forwarding
 sed -i "/^AllowTcpForwarding/c\AllowTcpForwarding yes" /etc/ssh/sshd_config
 sed -i "/^X11Forwarding/c\X11Forwarding yes" /etc/ssh/sshd_config
+sed -i "/^GatewayPorts/c\#GatewayPorts yes" /etc/ssh/sshd_config
 
