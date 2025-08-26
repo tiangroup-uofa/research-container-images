3533f0c412694b858e134953d00015011d394f58
T.Tian <alchem0x2a@gmail.com>
2025-08-25 23:14:20 -0600

    allow modify base dockerfile

---
+++ base/init_env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This script initializes the env variables that could be used for vast.ai services
+SCRIPT_NAME=$(realpath "$0")
+SCRIPT_PATH=$(dirname ${SCRIPT_NAME})
+source ${SCRIPT_PATH}/vastai.env
+if ! [ -z "$RCLONE_CONF_BASE64" ]
+then
+    if $(command -v rclone >/dev/null)
+    then
+	mkdir -p /home/$NB_USER/.config/rclone
+	echo $RCLONE_CONF_BASE64 | base64 -d >> /home/$NB_USER/.config/rclone/rclone.conf
+	fix-permissions /home/$NB_USER/.config
+    fi
+fi
+unset RCLONE_CONF_BASE64
+echo GITHUB_TOKEN=${GITHUB_TOKEN} >> /etc/environment
+echo WANDB_API_KEY=${WANDB_API_KEY} >> /etc/environment
+
+if $(command -v start-notebook.py >/dev/null)
+then
+    start-notebook.py --IdentityProvider.token=${JUPYTER_LAB_TOKEN} & 
+fi
+unset JUPYTER_LAB_TOKEN
